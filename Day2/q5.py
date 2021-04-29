5b5c9ce2a1abb8697f5e1d6c4e255991adfcbc77
Om Rawal <omrawal007@gmail.com>
2021-04-29 17:55:03 +0530

    Buy sell stock Optimal

--- Day2/q5.py
+++ Day2/q5.py
@@ -1,10 +1,13 @@
 # Buy sell stock
 
+# Time O(n^2)
+# Space O(1)
+
 
 def brute(arr):
     n = len(arr)
-    start = -1
-    end = -1
+    # start = -1
+    # end = -1
     max_profit = 0
     for i in range(n):
         curr_sum = arr[i]
@@ -16,3 +19,20 @@ def brute(arr):
 
 # arr = [7, 6, 4, 3, 1]
 # print(brute(arr))
+
+
+# Time O(n)
+# Space O(1)
+def optimal(arr):
+    min_val = 1000001
+    profit = 0
+    n = len(arr)
+    for i in range(n):
+        min_val = min(min_val, arr[i])
+        profit = max(profit, arr[i]-min_val)
+    return profit
+
+
+arr = [7, 1, 5, 3, 6, 4]
+
+print(optimal(arr))
