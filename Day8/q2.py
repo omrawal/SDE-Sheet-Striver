9f3f8f1b8fd9d1855c31e9435fd074afbc9bb7b9
Om Rawal <omrawal007@gmail.com>
2021-06-19 09:40:08 +0530

    Min Platform Solved

---
+++ Day8/q2.py
@@ -0,0 +1,26 @@
+# Minimum platforms required
+# sort both arrival and departure time
+# no need to care of sequence as we just need max platforms occupied simultaneously
+
+
+def minPlatform(n, arr, dep):
+    if(n < 2):
+        return n
+    arr.sort()
+    dep.sort()
+    platform_needed = 1
+    result = 1
+    i = 0
+    j = 0
+    while(i < n and j < n):
+        if(arr[i] <= dep[j]):
+            platform_needed += 1
+            i += 1
+        else:
+            platform_needed -= 1
+            j += 1
+        result = max(result, platform_needed)
+    return result
+
+# Time O(nlogn + nlogn)
+# space O(1)
