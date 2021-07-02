29db81dc1781ded99fca0c81bf2ea91877db0760
Om Rawal <omrawal007@gmail.com>
2021-07-02 10:17:22 +0530

    N queens complete

--- Day10/q2.py
+++ Day10/q2.py
@@ -2,6 +2,7 @@
 
 # queen no. is same as row no. +1
 # matric is chess board
+# import copy
 
 
 def transpose(matrix):
@@ -17,12 +18,24 @@ def transpose(matrix):
     return new_mat
 
 
+def modifyCopy(matrix):
+    n = len(matrix)
+    new_mat = []
+
+    for i in range(n):
+        res = ''
+        for j in range(n):
+            res += matrix[i][j]
+        new_mat.append(res)
+    return new_mat
+
+
 def isSafe(row, col, matrix):
     # as checking for newly placed queen is best decision
     # we only check if the newly added queen is under attack
     # Q denotes Queen
     # as matrix is nxn
-
+    n = len(matrix)
     # queens can only be in N,W,NW dirs
 
     # North
@@ -33,23 +46,77 @@ def isSafe(row, col, matrix):
             return False
         r -= 1
 
-    # West
-    r = row
-    c = col-1
-    while(c >= 0):
-        if(matrix[r][c] == 'Q'):
-            return False
-        c -= 1
-
     # North West
-    r = row-1
-    c = col-1
+    r = row
+    c = col
     while(c >= 0 and r >= 0):
         if(matrix[r][c] == 'Q'):
             return False
         c -= 1
         r -= 1
+
+    # West
+    r = row
+    c = col
+    while(c >= 0):
+        if(matrix[r][c] == 'Q'):
+            return False
+        c -= 1
+
+    # South West
+    r = row
+    c = col
+    while(r < n and c >= 0):
+        if(matrix[r][c] == 'Q'):
+            return False
+        r += 1
+        c -= 1
+
+    # # North East
+    # r = row+1
+    # c = col+1
+    # while(c < n and r < n):
+    #     if(matrix[r][c] == 'Q'):
+    #         return False
+    #     c += 1
+    #     r += 1
+
     return True
+
+
+def solve(col, matrix, ans):
+    n = len(matrix)
+    if(col == n):
+        # all queens are places and are safe
+        # ans.append(matrix.copy())
+        ans.append(modifyCopy(matrix))
+        # print(matrix.copy())
+        return
+    for row in range(0, n):
+        if(isSafe(row, col, matrix)):
+            # new queen safe so place it
+            matrix[row][col] = 'Q'
+            # print(row, col)
+            solve(col+1, matrix, ans)
+            # no safe place in this column for current placement backtrack
+            matrix[row][col] = '.'
+
+
+def nqueen(n):
+    # matrix = [['.']*n]*n
+    matrix = [['.' for i in range(n)] for j in range(n)]
+    ans = list()
+    solve(0, matrix, ans)
+    return ans
+
+
+print(nqueen(4))
+
+
+# k = [['.', '.', 'Q', '.'], ['Q', '.', '.', '.'],
+#      ['.', '.', '.', 'Q'], ['.', 'Q', '.', '.']]
+
+# print(modifyCopy(k))
 
 
 # matrix = [
@@ -58,12 +125,14 @@ def isSafe(row, col, matrix):
 #     '....',
 #     '....'
 # ]
-# matrix = [['.']*4]*4
+# matrix = [
+#     ['Q', '.', '.', '.'],
+#     ['.', '.', '.', '.'],
+#     ['.', '.', '.', '.'],
+#     ['.', '.', '.', '.']
+# ]
 # print(matrix)
-# print(isSafe(1, 2, matrix))
-# print(matrix[2][0])
-
-def nqueen(n):
-    matrix = [['.']*n]*n
-    for i in range(0, n):
-        pass
+# print(isSafe(1, 0, matrix))
+# for i in range(len(matrix)):
+#     for j in range(len(matrix)):
+#         print(i, j, matrix[i][j])
