64ec035679dcca06415bc8cd5c3e1f7dd266530d
Om Rawal <omrawal007@gmail.com>
2021-07-02 14:51:49 +0530

    Stacks using arrays complete

---
+++ Day13/q1.py
@@ -0,0 +1,25 @@
+# stack using arrays
+class MyStack:
+
+    def __init__(self):
+        self.arr = ['None' for i in range(100)]
+        self.pointer = -1
+    # Function to push an integer into the stack.
+
+    def push(self, data):
+        # add code here
+        self.arr[self.pointer+1] = data
+        self.pointer += 1
+
+    # Function to remove an item from top of the stack.
+    def pop(self):
+        # add code here
+        if(self.pointer == -1):
+            return self.pointer
+        else:
+            k = self.arr[self.pointer]
+            self.pointer -= 1
+            return k
+
+# Time O(1)
+# Space O(1) push and pop ops
